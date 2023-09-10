909128e5ae5fff206db65cf06c328e8ad4af6ab9
David Umoru <umorudavido@gmail.com>
2023-09-10 22:25:29 +0100

    Design user schema

---
+++ src/models/user.models.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  track: {
+    type: String,
+    required: true,
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

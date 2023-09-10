0521dbaddf984e002702bcdc4317e1be23d20efa
David Umoru <umorudavido@gmail.com>
2023-09-10 22:14:52 +0100

    Setup server

---
+++ src/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const connectDB = require("./configs/database");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// connecting to db here
+connectDB(process.env.MONGO_URI);
+
+// Defining a route for the root URL ("/")
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "I am running",
+  });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

2940bb7cfddce2310597ffb8db4edce16b4667bb
David Umoru <umorudavido@gmail.com>
2023-09-10 23:18:56 +0100

    Implement input validation and error handling

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,18 +1,31 @@
 const express = require("express");
+const { validationResult, body, param } = require("express-validator");
 const router = express.Router();
 
 const User = require("../models/user.models");
 
 // Create a new user
-router.post("/", async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
+router.post(
+  "/",
+  [
+    body("fullName").notEmpty().withMessage("Full name is required"),
+    body("track").notEmpty().withMessage("Track is required"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const newUser = new User(req.body);
+      await newUser.save();
+      res.status(201).json(newUser);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
-});
+);
 
 // Get all users
 router.get("/", async (req, res) => {
@@ -25,44 +38,75 @@ router.get("/", async (req, res) => {
 });
 
 // Get a specific user by ID
-router.get("/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+router.get(
+  "/:id",
+  [param("id").isMongoId().withMessage("Invalid user ID")],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
-});
+);
 
 // Update a user by ID
-router.put("/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+router.put(
+  "/:id",
+  [
+    param("id").isMongoId().withMessage("Invalid user ID"),
+    body("fullName").notEmpty().withMessage("Full name is required"),
+    body("track").notEmpty().withMessage("Track is required"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
-});
+);
 
 // Delete a user by ID
-router.delete("/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndRemove(req.params.id);
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+router.delete(
+  "/:id",
+  [param("id").isMongoId().withMessage("Invalid user ID")],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.json({ message: "User deleted" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+
+    try {
+      const user = await User.findByIdAndRemove(req.params.id);
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.json({ message: "User deleted" });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   }
-});
+);
 
 module.exports = router;

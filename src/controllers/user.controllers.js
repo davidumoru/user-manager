4e5b7f5215104160bc65c29d5bc0ba03a0c5a190
David Umoru <umorudavido@gmail.com>
2023-09-12 12:36:59 +0100

    Add success response

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -20,7 +20,8 @@ router.post(
     try {
       const newUser = new User(req.body);
       await newUser.save();
-      res.status(201).json(newUser);
+      // Success response: User created successfully
+      res.status(201).json({ message: "User created successfully", user: newUser });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
@@ -37,7 +38,8 @@ router.get(
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
-      res.json(user);
+      // Success response: User retrieved successfully
+      res.json({ message: "User retrieved successfully", user });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
@@ -69,7 +71,8 @@ router.put(
       if (!updatedUser) {
         return res.status(404).json({ message: "User not found" });
       }
-      res.json(updatedUser);
+      // Success response: User updated successfully
+      res.json({ message: "User updated successfully", user: updatedUser });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
@@ -86,7 +89,8 @@ router.delete(
       if (!deletedUser) {
         return res.status(404).json({ message: "User not found" });
       }
-      res.json({ message: "User deleted" });
+      // Success response: User deleted successfully
+      res.json({ message: "User deleted successfully" });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
